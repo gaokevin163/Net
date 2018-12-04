ee0bf99da660c655fde39da80a8584c48080e7a4
root <gaokevin163@gmail.com>
2018-12-04 11:06:51 +0800

    add

---
+++ door_jemalloc/src/hooks.pic.d
@@ -0,0 +1,8 @@
+src/hooks.pic.o: src/hooks.c \
+ include/jemalloc/internal/jemalloc_preamble.h \
+ include/jemalloc/internal/jemalloc_internal_defs.h \
+ include/jemalloc/internal/jemalloc_internal_decls.h \
+ include/jemalloc/internal/../jemalloc.h \
+ include/jemalloc/internal/jemalloc_internal_macros.h \
+ include/jemalloc/internal/private_namespace.h \
+ include/jemalloc/internal/hooks.h
---
+++ door_jemalloc/src/spin.sym.d
@@ -0,0 +1,6 @@
+src/spin.sym.o: src/spin.c include/jemalloc/internal/jemalloc_preamble.h \
+ include/jemalloc/internal/jemalloc_internal_defs.h \
+ include/jemalloc/internal/jemalloc_internal_decls.h \
+ include/jemalloc/internal/../jemalloc.h \
+ include/jemalloc/internal/jemalloc_internal_macros.h \
+ include/jemalloc/internal/hooks.h include/jemalloc/internal/spin.h
